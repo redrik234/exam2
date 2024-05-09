3a4093d37fb563f65b30a7a1db9a913b887d691d
redrik_dark <redrik_dark@dark.ru>
2024-05-09 21:14:09 +0300

    ex2-51

---
+++ local/php_interface/events/lang/ru/mail.php
@@ -0,0 +1,6 @@
+<?php
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+$MESS['UNAUTHORIZE_USER'] = 'Пользователь не авторизован, данные из формы: #FORM_USER_NAME#';
+$MESS['AUTHORIZE_USER'] = 'Пользователь авторизован: #ID# (#LOGIN#) #NAME#, данные из формы: #FORM_USER_NAME#';
+$MESS['CHANGE_AUTHOR_MSG'] = '«Замена данных в отсылаемом письме – #AUTHOR#';
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -3,3 +3,5 @@
 include_once(__DIR__ . "/events/iblock_element.php");
 
 include_once(__DIR__ . "/events/page.php");
+
+include_once(__DIR__ . "/events/mail.php");
