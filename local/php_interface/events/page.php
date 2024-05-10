ed29fb2937a7d82d4e9cfda6a8d063ad93fc94b6
redrik_dark <redrik_dark@dark.ru>
2024-05-10 15:47:53 +0300

    ex2-94

--- local/php_interface/events/page.php
+++ local/php_interface/events/page.php
@@ -2,8 +2,12 @@
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 
 AddEventHandler('main', 'OnEpilog', [PageHandler::class, 'notFound']);
+AddEventHandler('main', 'OnEpilog', [PageHandler::class, 'replaceMetaTag']);
 
 class PageHandler {
+
+    private const META_TAG_IBLOCK_CODE = 'metatags'; 
+
     public static function notFound() {
         if (!defined('ERROR_404') || ERROR_404 !== 'Y') {
             return true;
@@ -16,5 +20,31 @@ class PageHandler {
             "MODULE_ID" => "main",
             "DESCRIPTION" => $APPLICATION->GetCurPage(),
         ]);
+    }
+
+    public static function replaceMetaTag() {
+        global $APPLICATION;
+        $metaTagORM = CIBlockElement::GetList(
+            [],
+            [
+                'IBLOCK_CODE' => self::META_TAG_IBLOCK_CODE,
+                '=NAME' => $APPLICATION->GetCurPage(),
+                'ACTIVE' => 'Y'
+            ],
+            false,
+            false,
+            [
+                'ID', 'NAME', 'PROPERTY_TITLE', 'PROPERTY_DESCRIPTION'
+            ]
+        );
+        if ($data = $metaTagORM->Fetch()) {
+            if ($data['PROPERTY_TITLE_VALUE']) {
+                $APPLICATION->SetPageProperty('title', $data['PROPERTY_TITLE_VALUE']);
+            }
+
+            if ($data['PROPERTY_DESCRIPTION_VALUE']) {
+                $APPLICATION->SetPageProperty('description', $data['PROPERTY_DESCRIPTION_VALUE']);
+            }
+        }
     }
 }
